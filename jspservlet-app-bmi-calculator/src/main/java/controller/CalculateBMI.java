61f328be6f5fb936522a60c1a7f40c982382194a
Iamneo <techadmin@iamneo.ai>
2023-09-06 13:23:29 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -3,7 +3,9 @@ package controller;
 public class CalculateBMI {
 
 	public static double calculate(double height, double weight){
-		return height/weight;
+		height = height/100;
+		double BMI = weight/(height*height);
+		return BMI;
 	}
 	public static String description(double bmi){
 		if(bmi < 18.5) return "Under Weight";
